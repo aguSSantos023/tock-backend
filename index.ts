efe25b75786a7a0ba89fdfac87bbcaf370c7b496
AguSSantos Prieto Atienza <131393036+aguSSantos023@users.noreply.github.com>
2026-02-05 09:51:53 +0100

    feat: implement register endpoint & downgrade prisma to v5.10

--- index.ts
+++ index.ts
@@ -1,11 +1,16 @@
 import express from "express";
+import authRoutes from "./routes/auth.routes";
 
 const app = express();
 const port = process.env.PORT;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hola desde el back 🐳");
 });
+
+app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
