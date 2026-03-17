db194dfae7ddcff4d697d0c284e14a065a2fb477
AguSSantos Prieto Atienza <131393036+aguSSantos023@users.noreply.github.com>
2026-03-17 15:33:15 +0100

    test: implement unit test checkLoginStatus of config.middleware

--- src/middlewares/config.middleware.test.ts
+++ src/middlewares/config.middleware.test.ts
@@ -1,6 +1,6 @@
 import { prisma } from "../config/db";
 import { beforeEach, describe, expect, it, vi } from "vitest";
-import { checkRegisterStatus } from "./config.middleware";
+import { checkLoginStatus, checkRegisterStatus } from "./config.middleware";
 
 vi.mock("../config/db", () => ({
   prisma: {
@@ -47,6 +47,49 @@ describe("middleware checkRegisterStatus", () => {
     });
 
     await checkRegisterStatus(mockReq, mockRes, nextFunction);
+
+    expect(nextFunction).toHaveBeenCalled();
+    expect(mockRes.status).not.toHaveBeenCalledWith();
+    expect(mockRes.json).not.toHaveBeenCalledWith();
+  });
+});
+
+describe("middleware checkLoginStatus", () => {
+  let mockReq: any;
+  let mockRes: any;
+  let nextFunction: any;
+
+  beforeEach(() => {
+    vi.restoreAllMocks();
+
+    mockReq = {};
+    mockRes = {
+      status: vi.fn().mockReturnThis(),
+      json: vi.fn(),
+    };
+    nextFunction = vi.fn();
+  });
+
+  it("debería devolver 503 si el inicio de sesíon está bloqueado", async () => {
+    (prisma.systemConfig.findUnique as any).mockResolvedValue({
+      is_login_blocked: true,
+    });
+
+    await checkLoginStatus(mockReq, mockRes, nextFunction);
+
+    expect(mockRes.status).toHaveBeenCalledWith(503);
+    expect(mockRes.json).toHaveBeenCalledWith({
+      message: "El inicio de sesión está deshabilitado.",
+    });
+    expect(nextFunction).not.toHaveBeenCalled();
+  });
+
+  it("debería  llamar a next() si el inicio de sesíon no está bloqueado", async () => {
+    (prisma.systemConfig.findUnique as any).mockResolvedValue({
+      is_login_blocked: false,
+    });
+
+    await checkLoginStatus(mockReq, mockRes, nextFunction);
 
     expect(nextFunction).toHaveBeenCalled();
     expect(mockRes.status).not.toHaveBeenCalledWith();
